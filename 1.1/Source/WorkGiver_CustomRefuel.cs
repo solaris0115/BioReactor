9775cbe93a39b549ac4523214e52625e66e9d335
jhjjgu0115 <jhjjgu0115@naver.com>
2020-05-03 17:47:45 +0900

    clean up

---
+++ 1.1/Source/WorkGiver_CustomRefuel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Verse.AI;
+using Verse;
+
+
+namespace BioReactor
+{
+    public class WorkGiver_CustomRefuel : WorkGiver_Scanner
+    {
+        public override ThingRequest PotentialWorkThingRequest
+        {
+            get
+            {
+                
+                ThingRequest thingRequest = ThingRequest.ForGroup(ThingRequestGroup.BuildingArtificial);
+                return thingRequest;
+            }
+        }
+
+        public override PathEndMode PathEndMode
+        {
+            get
+            {
+                return PathEndMode.Touch;
+            }
+        }
+
+        public virtual JobDef JobStandard
+        {
+            get
+            {
+                return JobDefOf.Refuel;
+            }
+        }
+
+        public virtual JobDef JobAtomic
+        {
+            get
+            {
+                return JobDefOf.RefuelAtomic;
+            }
+        }
+
+        public virtual bool CanRefuelThing(Thing t)
+        {
+            return !(t is Building_Turret);
+        }
+
+        public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            return CanRefuelThing(t) && RefuelWorkGiverUtility.CanRefuel(pawn, t, forced);
+        }
+
+        public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            return RefuelWorkGiverUtility.RefuelJob(pawn, t, forced, JobStandard, JobAtomic);
+        }
+    }
+}
