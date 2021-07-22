2c0fe8ce1a9200f98b0e48e835a58c516ad6719e
solaris0115 <solaris0115@naver.com>
2021-07-23 01:06:54 +0900

    Folder struct redesign

---
+++ Symbolic_link.bat
@@ -0,0 +1,3 @@
+rmdir "E:\Program Files (x86)\SteamLibrary\steamapps\common\RimWorld\Mods\BioReactor"
+mklink /d  "E:\Program Files (x86)\SteamLibrary\steamapps\common\RimWorld\Mods\BioReactor" E:\GitProject\BioReactor\BioReactor
+pause
